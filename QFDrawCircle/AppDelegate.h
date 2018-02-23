09ddbf71a69c3b6bd29bf80b2df60c9b5f23ae0e
流年释残忆 <291753906@qq.com>
2018-02-23 16:08:56 +0800

    Initial Commit

---
+++ QFDrawCircle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QFDrawCircle
+//
+//  Created by iosyf-02 on 2018/2/23.
+//  Copyright © 2018年 情风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
